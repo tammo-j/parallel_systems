a1d1b8fdb1491c667c99fff4e08add361a05a87a
Rico Jasper <rjasper@mailbox.tu-berlin.de>
2013-05-08 13:06:46 +0200

    modified 2-2 conways game of life variant B serial naive improved rule checking algorithm

--- lab_2/Rico/src/conway_algorithm_serial_b.c
+++ lab_2/Rico/src/conway_algorithm_serial_b.c
@@ -20,17 +20,12 @@ void tick(map *map, int t) {
 /*	printf("tick %d\n", t); */
 
 	int neighbors[map->width][map->height];
-	bool cells[map->width][map->height];
 	memset(neighbors, 0, sizeof(neighbors));
-	memset(cells, false, sizeof(cells));
-
-	map_restart(map, t);
 	
 	int x, y;
-	
+
+	map_restart(map, t);
 	while (map_next(map, t, &x, &y)) {
-		cells[x][y] = true;
-	
 		// for all neighboring cells
 		for (int dx = -1; dx <= 1; ++dx) {
 			for (int dy = -1; dy <= 1; ++dy) {
@@ -41,15 +36,50 @@ void tick(map *map, int t) {
 			}
 		}
 	}
+	
+	// for all live cells
+	// check and apply survival rule
+	map_restart(map, t);
+	while (map_next(map, t, &x, &y)) {
+		int n = neighbors[x][y];
 		
-	// check condition
-	for (int ix = 0; ix < map->width; ++ix) {
-		for (int iy = 0; iy < map->height; ++iy) {
-			int n = neighbors[ix][iy];
+		if (n >= 2 && n <= 3)
+			map_append(map, t+1, x, y);
+		
+		neighbors[x][y] = 0;
+	}
+	
+	// for all live cells
+	map_restart(map, t);
+	while (map_next(map, t, &x, &y)) {
+		// for dead neighboring cells
+		// check and apply birth rule
+		for (int dx = -1; dx <= 1; ++dx) {
+			for (int dy = -1; dy <= 1; ++dy) {
+				if (dx == 0 && dy == 0)
+					continue;
+				
+				int nx = x + dx;
+				int ny = y + dy;
 			
-			if (n == 3 || (cells[ix][iy] && n == 2))
-				map_append(map, t+1, ix, iy);
+				int n = neighbors[nx][ny];
+		
+				if (n == 3)
+					map_append(map, t+1, nx, ny);
+				
+				neighbors[nx][ny] = 0;
+			}
 		}
 	}
+		
+//	// check condition
+//	for (int ix = 0; ix < map->width; ++ix) {
+//		for (int iy = 0; iy < map->height; ++iy) {
+//			int n = neighbors[ix][iy];
+//			
+//			if (n == 3 || (cells[ix][iy] && n == 2))
+//				map_append(map, t+1, ix, iy);
+//		}
+//	}
 }
 
