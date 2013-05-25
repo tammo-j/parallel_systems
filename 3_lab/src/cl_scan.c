bc2780a29d099b43784d7ef5f4256f87cb8bb21f
Rico Jasper <rjasper@mailbox.tu-berlin.de>
2013-05-25 16:39:01 +0200

    fixed major bug (global barrier is not available)

--- 3_lab/src/cl_scan.c
+++ 3_lab/src/cl_scan.c
@@ -5,11 +5,12 @@
 
 #include <CL/opencl.h>
 
-#define NUM_GROUPS          3
-#define NUM_ITEMS_PER_GROUP 3
-#define NUM_ELEMS_PER_ITEM  10
-#define NUM_TOTAL_ITEMS     NUM_GROUPS * NUM_ITEMS_PER_GROUP
-#define NUM_TOTAL_ELEMS     NUM_ELEMS_PER_ITEM * NUM_TOTAL_ITEMS
+#define NUM_ELEMS_PER_ITEM  15
+#define NUM_ITEMS_PER_GROUP 4
+#define NUM_GROUPS          5
+#define NUM_ELEMS_PER_GROUP NUM_ELEMS_PER_ITEM  * NUM_ITEMS_PER_GROUP
+#define NUM_ITEMS     NUM_ITEMS_PER_GROUP * NUM_GROUPS 
+#define NUM_ELEMS     NUM_ELEMS_PER_GROUP * NUM_GROUPS
 
 void printBuildError(cl_program program, cl_device_id device)
 {
@@ -82,42 +83,71 @@ int main(int argc, char **argv)
 		printBuildError(program, mydevice);
 	assert(status == CL_SUCCESS);
 	
-	cl_kernel kernel = clCreateKernel(program, "cumulate", NULL);
-	assert(kernel != NULL);
+	cl_kernel cumulate = clCreateKernel(program, "cumulate", NULL);
+	cl_kernel add_offset = clCreateKernel(program, "add_offset", NULL);
+	assert(cumulate != NULL);
+	assert(add_offset != NULL);
 
 	// Create memory objects
-	cl_mem cl_arr;
-	cl_arr = clCreateBuffer(ctx, CL_MEM_READ_WRITE, NUM_TOTAL_ELEMS * sizeof(cl_int), NULL, NULL);
+	cl_mem cl_arr, cl_item_offsets, cl_group_offsets;
+	cl_arr     = clCreateBuffer(ctx, CL_MEM_READ_WRITE, NUM_ELEMS * sizeof(cl_int), NULL, NULL);
+	cl_item_offsets = clCreateBuffer(ctx, CL_MEM_READ_WRITE, NUM_ITEMS * sizeof(cl_int), NULL, NULL);
+	cl_group_offsets = clCreateBuffer(ctx, CL_MEM_READ_ONLY, NUM_ITEMS * sizeof(cl_int), NULL, NULL);
+	
 	
 	// Prepare data on host + transfer to device
-	cl_int arr[NUM_TOTAL_ELEMS];
+	cl_int arr[NUM_ELEMS];
+	cl_int item_offsets[NUM_ITEMS];
+	cl_int group_offsets[NUM_GROUPS];
 
-	for (i = 0; i < NUM_TOTAL_ELEMS; ++i)
+	for (i = 0; i < NUM_ELEMS; ++i)
 		arr[i] = 1;
 	
-	clEnqueueWriteBuffer(cq, cl_arr, CL_TRUE, 0, NUM_TOTAL_ELEMS * sizeof(cl_int), arr, 0, NULL, NULL);
+	clEnqueueWriteBuffer(cq, cl_arr, CL_TRUE, 0, NUM_ELEMS * sizeof(*arr), arr, 0, NULL, NULL);
 
 	cl_int elems_per_item = NUM_ELEMS_PER_ITEM;
 
 	// Bind kernel arguments + execute kernel
-	clSetKernelArg(kernel, 0, sizeof(cl_mem), &cl_arr);
+	clSetKernelArg(cumulate, 0, sizeof(cl_mem), &cl_arr);
+	clSetKernelArg(cumulate, 1, sizeof(cl_mem), &cl_item_offsets);
+	clSetKernelArg(add_offset, 0, sizeof(cl_mem), &cl_arr);
+	clSetKernelArg(add_offset, 1, sizeof(cl_mem), &cl_item_offsets);
+	clSetKernelArg(add_offset, 2, sizeof(cl_mem), &cl_group_offsets);
 	
-	size_t global_work_sizes[] = { NUM_TOTAL_ITEMS };
+	size_t global_work_sizes[] = { NUM_ITEMS };
 	const size_t local_work_sizes[] = { NUM_ITEMS_PER_GROUP };
-	status = clEnqueueNDRangeKernel(cq, kernel, 1, NULL, global_work_sizes, local_work_sizes, 0, NULL, NULL);
+	status = clEnqueueNDRangeKernel(cq, cumulate, 1, NULL, global_work_sizes, local_work_sizes, 0, NULL, NULL);
+	assert(status == CL_SUCCESS);
+	clFinish(cq);
+
+	clEnqueueReadBuffer(cq, cl_item_offsets, CL_TRUE, 0, NUM_ITEMS * sizeof(*item_offsets), item_offsets, 0, NULL, NULL);
+	int acc = 0;
+	for (int i = 0; i < NUM_GROUPS; ++i) {
+		acc += item_offsets[(i+1)*NUM_ITEMS_PER_GROUP-1];
+		group_offsets[i] = acc;
+
+		printf("group %d: %d\n", i, acc);
+	}
+
+	clEnqueueWriteBuffer(cq, cl_group_offsets, CL_TRUE, 0, NUM_GROUPS * sizeof(*group_offsets), group_offsets, 0, NULL, NULL);
+
+	status = clEnqueueNDRangeKernel(cq, add_offset, 1, NULL, global_work_sizes, local_work_sizes, 0, NULL, NULL);
 	assert(status == CL_SUCCESS);
 	clFinish(cq);
 	
 	// Transfer results back + verify
-	clEnqueueReadBuffer(cq, cl_arr, CL_TRUE, 0, NUM_TOTAL_ELEMS * sizeof(*arr), arr, 0, NULL, NULL);
+	clEnqueueReadBuffer(cq, cl_arr, CL_TRUE, 0, NUM_ELEMS * sizeof(*arr), arr, 0, NULL, NULL);
 	
-	for (i = 0; i < NUM_TOTAL_ELEMS; i++) {
+	for (i = 0; i < NUM_ELEMS; i++) {
 		printf("%d: %d\n", i, arr[i]);
 	}
 	
 	// Free everything
 	clReleaseMemObject(cl_arr);
-	clReleaseKernel(kernel);
+	clReleaseMemObject(cl_item_offsets);
+	clReleaseMemObject(cl_group_offsets);
+	clReleaseKernel(cumulate);
+	clReleaseKernel(add_offset);
 	clReleaseProgram(program);
 	clReleaseCommandQueue(cq);
 	clReleaseContext(ctx);
--- 3_lab/src/kernel.cl
+++ 3_lab/src/kernel.cl
@@ -1,16 +1,11 @@
 
-#define NUM_ELEMS_PER_ITEM  10
-#define NUM_ITEMS_PER_GROUP 3
-#define NUM_GROUPS          3
-#define NUM_ELEMS_PER_GROUP NUM_ELEMS_PER_ITEM  * NUM_ITEMS_PER_GROUP
-#define NUM_TOTAL_ITEMS     NUM_ITEMS_PER_GROUP * NUM_GROUPS 
-#define NUM_TOTAL_ELEMS     NUM_ELEMS_PER_GROUP * NUM_GROUPS
+#define NUM_ELEMS_PER_ITEM  15
+#define NUM_ITEMS_PER_GROUP 4
+#define NUM_GROUPS          5
 
-__kernel void cumulate(__global int *arr) {
-	// private cumulated sum of this work item
-	__private int item_sum [NUM_ELEMS_PER_ITEM ];
+__kernel void cumulate(__global int *arr, __global int *item_offsets) {
 	// local cumulated sum of this work group
-	__local   int group_sum[NUM_ITEMS_PER_GROUP];
+	__local int group_sum[NUM_ITEMS_PER_GROUP];
 	
 	// some ids
 	int global_id = get_global_id(0);
@@ -18,19 +13,15 @@ __kernel void cumulate(__global int *arr) {
 	int group_id = get_group_id(0);
 	
 	// starting point of the input array for this work item
-	int offset = global_id * NUM_ELEMS_PER_ITEM;
-	
-	// accumulator
-	int acc;
+	int arr_offset = global_id * NUM_ELEMS_PER_ITEM;
 	
 	// STEP 1: scan on work item level
 
 	// cumulate sum on work item level
-	acc = 0;
-	for (int i = 0; i < NUM_ELEMS_PER_ITEM; ++i) {
-		acc += arr[offset + i];
-		item_sum[i] = acc;
-	}
+	int acc = 0;
+	for (int i = 0; i < NUM_ELEMS_PER_ITEM; ++i)
+		acc = arr[arr_offset + i] += acc;
+	
 	// save total sum of this work item
 	group_sum[local_id] = acc;
 	
@@ -44,47 +35,26 @@ __kernel void cumulate(__global int *arr) {
 		acc = 0;
 		for (int i = 0; i < NUM_ITEMS_PER_GROUP; ++i) {
 			acc += group_sum[i];
-			group_sum[i] = acc;
-		}
-		// save the total sum of this work group
-
-		// Use the arr array as storage so
-		// we don't need to get extra global memory.
-		// Write at the first position of each group.
-		arr[group_id * NUM_ELEMS_PER_GROUP] = acc;
-	}
-	
-	barrier(CLK_GLOBAL_MEM_FENCE);
-
-	// STEP 3: scan on device level
-
-	// for one item on this device
-	if (global_id == 0) {
-		// cumulate the total sum of each work group
-		acc = 0;
-		for (int i = 0; i < NUM_TOTAL_ELEMS; i += NUM_ELEMS_PER_GROUP) {
-			acc += arr[i];
-			arr[i] = acc;
+			item_offsets[group_id*NUM_ITEMS_PER_GROUP + i] = acc;
 		}
 	}
-	
-	barrier(CLK_GLOBAL_MEM_FENCE);
-
-	// STEP 4: merging results
-	
-	// offset of the sum of this work item to be added
-	int sum_offset = 0;
-	
-	// add offset of this group
-	if (group_id > 0)
-		sum_offset += arr[(group_id-1) * NUM_ELEMS_PER_GROUP];
-	// add the offset of this work item
-	if (local_id > 0)
-		sum_offset += group_sum[local_id-1];
-	
-	// add the overall offset to the private sum
-	// and write back to input array
-	for (int i = 0; i < NUM_ELEMS_PER_ITEM; ++i)
-		arr[offset + i] = sum_offset + item_sum[i];
 }
 
+__kernel void add_offset(__global int *arr, __global int *item_offsets, __global int *group_offsets) {
+	int global_id  = get_global_id(0);
+	int group_id   = get_group_id(0);
+	int local_id   = get_local_id(0);
+	int arr_offset = global_id * NUM_ELEMS_PER_ITEM;
+	
+	// STEP 3: merge results
+
+	int offset = 0;
+
+	if (local_id > 0)
+		offset += item_offsets[global_id-1];
+	if (group_id > 0)
+		offset += group_offsets[group_id-1];
+
+	for (int i = 0; i < NUM_ELEMS_PER_ITEM; ++i)
+		arr[arr_offset + i] += offset;
+}
