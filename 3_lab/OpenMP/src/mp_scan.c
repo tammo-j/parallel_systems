ab083b03c63401e45b16d20a82a5c83c5779d92e
Rico Jasper <rjasper@mailbox.tu-berlin.de>
2013-05-27 12:07:11 +0200

    bugfix of OpenMP scan implementation

--- 3_lab/OpenMP/src/mp_scan.c
+++ 3_lab/OpenMP/src/mp_scan.c
@@ -1,43 +1,44 @@
-#include <omp.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <omp.h>
 #include <math.h>
 
-main ()  {
-	int i, tid, iCalc, chunk, iSerialCount, iParallelCount;
-
-	int iInput[9]= { 0 , 1, 2, 3, 4, 5, 6, 7, 8 };
-	chunk = 2;
-
-	omp_set_dynamic(0);
-	omp_set_num_threads(omp_get_max_threads());
-
-	for (iSerialCount = 1; iSerialCount <= 3; iSerialCount++)
-	{
-#pragma omp parallel shared(iInput,iSerialCount,chunk) private(iParallelCount, tid, iCalc)
-	{
-
-	#pragma omp for schedule(dynamic,chunk) nowait
-		for (iParallelCount=1; iParallelCount <= 8; iParallelCount++)
-		{
-			iCalc = pow(2.0,iSerialCount-1);
-			if ( iParallelCount  >= iCalc )
-			{
-				tid = omp_get_thread_num();
-				printf("ThreadNumber: %d\n", tid);
-				printf("SerielCount: %d\n", iSerialCount);
-				printf("2^(iSerialCount-1): %d\n", iCalc);
-				printf("ParallelCount: %d\n", iParallelCount);
-				printf("Input before computation: %d\n", iInput[iParallelCount]);
-
-				iInput[iParallelCount] = iInput[(iParallelCount - iCalc)] + iInput[iParallelCount];
-
-				printf("Input after computation: %d\n", iInput[iParallelCount]);
-				printf("\n");
-			}
-		}
+int main (int argc, char **argv)  {
+	int arr[] = {0, 1, 1, 1, 1, 1, 1, 1, 1};
+	int len = sizeof(arr) / sizeof(*arr);
+	
+	int temp[len];
+	int *src = arr;
+	int *dst = temp;
+	
+	memset(temp, 0, sizeof(temp));
+	
+	int n = len-1;
+	int m = floor( log(n) / log(2.) );
+	
+	printf("n=%d, m=%d\n", n, m);
+	
+	for (int i = 1; i <= m; ++i) {
+		int offset = floor( pow(2., i-1) );
+		
+		#pragma omp parallel for
+		for (int j = 1; j <= n; ++j) {
+			if (j < offset)
+				dst[j] = src[j];
+			else
+				dst[j] = src[j-offset] + src[j];
 			
-	}/* end of parallel section */
-	}/* end of serial for */
-
-	scanf("%d",&i);
+//			printf("%d: dst[%d] = src[%d] + src[%d] == %d = %d + %d\n", omp_get_thread_num(), j, j-offset, j, dst[j], src[j-offset], src[j]);
+		}
+		
+		int *swap_temp = src;
+		src = dst;
+		dst = swap_temp;
+	}
+	
+	for (int i = 0; i < len; ++i)
+		printf("arr[%d] = %d\n", i, src[i]);
+	
+	return EXIT_SUCCESS;
 }
